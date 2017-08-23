621b3902a43f6fb1e5a0c0c6210feea678044110
Rb2030 <rossmail2050@gmail.com>
2017-08-23 15:28:53 +0100

    Edited Constraints.

---
+++ Swoosh/WelcomeVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Ross on 21/08/2017.
+//  Copyright © 2017 Training. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+    }
+    
+    @IBAction func unwindToLeague(unwindSegue: UIStoryboardSegue) {
+    }
+
+}
+
